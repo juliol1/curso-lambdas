044f778c7fba90bc89809f7ce869adcd38ba1433
juliol1 <juliovillalvazo26@gmail.com>
2021-10-11 23:24:44 -0500

    Agregue el archivo de debugging.py ahorcado.py archivos.py

--- debugging.py
+++ debugging.py
@@ -1,15 +1,23 @@
 def divisors(num):
-    divisors = []
-    for i in range(1, num + 1):
-        if num % i == 0:
-            divisors.append(i)
-    return divisors
+    try:
+        if num < 0:
+            raise ValueError('Solo puedes ingresar valores positivos')
+        divisors = []
+        for i in range(1, num + 1):
+            if num % i == 0:
+                divisors.append(i)
+        return divisors
+    except ValueError as ve:
+        return ve           
 
 
 def run():
-    num = int(input("ingresa un numero: "))
-    print(divisors(num))
-    print("Termino mi programa")
+    try:
+        num = int(input("ingresa un numero: "))
+        print(divisors(num))
+        print("Termino mi programa")
+    except ValueError:
+        print("Debes ingresar un número entero")
 
 
 if __name__ == '__main__':
